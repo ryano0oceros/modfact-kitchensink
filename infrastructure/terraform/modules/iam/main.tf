fc6c9f1874c9ff70a347fc50814e238368bcd471
Ryano0oceros <getryanfallon@gmail.com>
2025-03-22 15:10:00 -0400

    Updated the OIDC provider's client_id_list and trust policy

--- infrastructure/terraform/modules/iam/main.tf
+++ infrastructure/terraform/modules/iam/main.tf
@@ -12,7 +12,7 @@ resource "aws_iam_role" "github_actions" {
         }
         Condition = {
           StringEquals = {
-            "token.actions.githubusercontent.com:aud": "https://token.actions.githubusercontent.com"
+            "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
           },
           StringLike = {
             "token.actions.githubusercontent.com:sub": "repo:${var.github_org}/${var.github_repo}:*"
@@ -62,7 +62,7 @@ resource "aws_iam_role_policy" "github_actions" {
 resource "aws_iam_openid_connect_provider" "github_actions" {
   url = "https://token.actions.githubusercontent.com"
 
-  client_id_list = ["https://token.actions.githubusercontent.com"]
+  client_id_list = ["sts.amazonaws.com"]
 
   thumbprint_list = [
     "6938fd4d98bab03faadb97b34396831e3780aea1"
